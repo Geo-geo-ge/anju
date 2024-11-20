5d646b22c192d9abcfd9ec75fad0698de6eb9a31
lithiyamathew <lithiyamathew192002@gmail.com>
2024-11-20 08:09:53 +0530

    first commit

---
+++ first/src/pro/pro2.java
@@ -0,0 +1,18 @@
+package pro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class pro2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\SWT\\Desktop\\sel\\chromedrive.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("");
+		driver.findElement(By.id("")).sendKeys("oo,ol,");
+		driver.findElement(By.id("")).sendKeys("535524");
+		driver.findElement(By.id("")).click();
+	}
+
+}
